41d51dcda0a8dfe559e0f10832755936cce2d760
Sam Havens <samhavens@gmail.com>
2018-05-03 01:21:13 -0700

    separate frontend works
    The JavaScript is very minimal, but enough to read the information in the fields, and send a request to the backend. Bokeh plots are not implemented, because I didn't want to send all that info over the network for a not-so-interesting graph.
    
    If we need a visualization, can we do something with the pages text

---
+++ static/js/main.js
@@ -0,0 +1,55 @@
+// This is ugly javascript, but it will work on IE10 and above
+
+var mainDiv = document.getElementById('main');
+var iframe = document.createElement('iframe');
+iframe.width = 960
+iframe.height = 700
+iframe.align = "middle"
+
+function encodeData(data) {
+  return Object.keys(data).map(function(key) {
+      return [key, data[key]].map(encodeURIComponent).join("=");
+  }).join("&");
+}
+
+function successHandler(res, iframe, mainDiv) {
+  var data = JSON.parse(res);
+  // console.log(data)
+  if (data && data[2] && data[2].file) {
+    dats = data[2]
+    var src = `https://s3-us-west-2.amazonaws.com/huaherokupdfs/${dats.make}/${dats.model}/${dats.year}/${dats.file}#page=${dats.page}`
+    iframe.src = src
+  }
+  
+  mainDiv.appendChild(iframe)
+}
+
+function mainHandler() {
+  var query = document.getElementById('query').value
+  var make = 'KIA'
+  var model = document.getElementById('model').value
+  var year = document.getElementById('year').value
+  var manual_type = document.getElementById('manual_type').value
+  var params = {query, make, model, year, manual_type}
+  var url = `api?${encodeData(params)}`
+
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      successHandler(this.response, iframe, mainDiv)
+    } else {
+      console.error(JSON.parse(this.response))
+    }
+  };
+
+  request.onerror = function() {
+    console.error('connection error of some sort')
+  };
+
+  request.send();
+  return false; // prevent the button from doing button stuff
+}
+
+document.getElementById('submit').onclick = mainHandler
